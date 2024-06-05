92a4616233e2fc4ddcbd2e51d9337f9533c4de9a
MagametAnchok <112862870+MagametAnchok@users.noreply.github.com>
2024-06-05 18:27:37 +0300

    Add files via upload

---
+++ docker/users_app/main.py
@@ -0,0 +1,35 @@
+import psycopg2
+
+# Параметры подключения к базе данных PostgreSQL
+DB_NAME = "kubsu"
+DB_USER = "kubsu"
+DB_PASSWORD = "kubsu"
+DB_HOST = "host.docker.internal"
+DB_PORT = "5432"
+
+def fetch_users():
+    try:
+        connection = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        
+        cursor = connection.cursor()
+        
+        cursor.execute("SELECT * FROM users")
+        
+        users = cursor.fetchall()
+        
+        for user in users:
+            print(*user)
+        
+        cursor.close()
+        connection.close()
+        
+    except (Exception, psycopg2.Error) as error:
+        print("Ошибка при работе с PostgreSQL:", error)
+
+fetch_users()
